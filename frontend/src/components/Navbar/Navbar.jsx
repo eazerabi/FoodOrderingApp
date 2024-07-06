a3e0aed78dc7f6eebfbe64de3d8092c40175b751
Abishanan Ganeshalingam <redishberry5@gmail.com>
2024-07-06 14:29:21 +0530

    First commit

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react'
+import './Navbar.css'
+import {assets} from '../../assets/assets'
+const Navbar = () => {
+
+ const[menu,setMenu] = useState("menu");
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="logo" />
+      <ul className="navbar-menu">
+        <li onClick={()=> setMenu('home')} className={menu==="home"?"active":""}>home</li>
+        <li onClick={()=> setMenu('menu')} className={menu==="menu"?"active":""}>menu</li>
+        <li onClick={()=> setMenu('mobile-app')} className={menu==="mobile-app"?"active":""}>mobile-app</li>
+        <li onClick={()=> setMenu('contact-us')} className={menu==="contact-us"?"active":""}>contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <nav className="navbar-search-ion">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </nav>
+        <button> Sign</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
