5c56a7817df054dee5ac4cf86ab5d87933b4b103
Abishanan Ganeshalingam <redishberry5@gmail.com>
2024-07-06 23:16:22 +0530

    Continusly developing website working on explore menu

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <ExploreMenu />
+        </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const PlaceOrder = () => {
+  return (
+    <div>PlaceOrder</div>
+  )
+}
+
+export default PlaceOrder
