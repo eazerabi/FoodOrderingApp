b83c76ca29328c8e29ba2df271057dbcbcbb0c67
Abishanan Ganeshalingam <redishberry5@gmail.com>
2024-07-07 15:50:59 +0530

    workin with food item and display

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-
+import{food_list} from "../assets/assets"
 export const StoreContext = createContext(null)
 
 
@@ -8,7 +8,7 @@ const StoreContextProvider = (props)=>{
 
 
     const contextValue ={
-        
+            food_list
     }
     return(
         <StoreContext.Provider value={contextValue}>
