612e18e48410462012e4aa5f74f01c713798f473
Abishanan Ganeshalingam <redishberry5@gmail.com>
2024-07-08 19:04:04 +0530

    updating

---
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './LoginPopup.css'
+
+const LoginPopup = () => {
+  return (
+    <div className='login-popup'>
+        Login
+    </div>
+  )
+}
+
+export default LoginPopup
